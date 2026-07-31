37e250605b221fb61dcd6241222a2559285edd02
jacobbyrne72 <byrnejacob68@gmail.com>
2026-07-31 04:16:42 +0000

    chore: bump version to 0.5.4 [v0.5.4]

--- forgeos/__init__.py
+++ forgeos/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 from .compiler import CompilerError, Mission, compile_mission
 from .circuit_breaker import BreakerState, CircuitBreaker
 from .prompt_cache import CacheEntry, PromptCache
